ca4f32be6ee07eca595a69cfafb33dc2776736f0
Gir <deniskot.davydov1991@gmail.com>
2025-11-13 02:01:13 +0300

    Тест спит 5 мин

--- download_cron.py
+++ download_cron.py
@@ -6,7 +6,7 @@ from integrations.downloader import run_download
 
 def _sleep_until_next_hour():
     now = datetime.now()
-    nxt = (now.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1))
+    nxt = (now.replace(minute=5, second=0, microsecond=0) + timedelta(hours=0))
     delta = (nxt - now).total_seconds()
     logger.info(f"⏰ Сплю {int(delta)} сек до следующего часа ({nxt.strftime('%H:%M')})")
     time.sleep(max(1, int(delta)))
