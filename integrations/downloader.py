0afb4cbe3d6491cb397e9945f52e4cd36c5a9a99
Gir <deniskot.davydov1991@gmail.com>
2025-11-12 13:20:07 +0300

    Добавил sys в downloader_2

--- integrations/downloader.py
+++ integrations/downloader.py
@@ -1,17 +1,19 @@
 # integrations/downloader.py
+import os
+import sys
+
+# Добавляем корень проекта в PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from playwright.sync_api import sync_playwright
 from datetime import datetime, timedelta
-import os
 import time
-import sys
 from utils.logger import logger
 from integrations.dropbox_watcher import upload_file
 from main import process_file
 from run_once_guard import acquire_lock, release_lock  # если у тебя уже есть этот модуль-сторож
 from integrations.telegram_bot import send_message_sync
 
-# Добавляем корень проекта в PYTHONPATH
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 LOGIN = os.getenv("ANTARES_LOGIN")
 PASSWORD = os.getenv("ANTARES_PASSWORD")
